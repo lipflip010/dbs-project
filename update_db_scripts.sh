31851685654cbb9ce99c6df4fefd25b7aaff1a22
lipflip010 <lipflip@posteo.de>
2021-06-10 14:05:07 +0200

    Launching container in detached mode

--- update_db_scripts.sh
+++ update_db_scripts.sh
@@ -1,3 +1,3 @@
 sudo docker-compose -f docker-compose.yml down
 sudo docker-compose -f docker-compose.yml build --no-cache
-sudo docker-compose -f docker-compose.yml up
+sudo docker-compose -f docker-compose.yml up -d

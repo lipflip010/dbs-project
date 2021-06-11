023b6a5aee4ff7315e2831ee05038a29b92e149c
lipflip010 <lipflip@posteo.de>
2021-06-11 18:32:03 +0200

    Added parameter country

--- web/src/app.py
+++ web/src/app.py
@@ -11,28 +11,15 @@ app = Flask(__name__)
 cache = redis.Redis(host='redis', port=6379)
 
 
-@app.route('/population-total')
-def population_total():
-    query_result = database.execute_query(
-        """SELECT year,count FROM population_total WHERE country_name='Germany' LIMIT 100""")
-    year,count = zip(*query_result)
+@app.route('/population-total/<country>')
+def population_total(country='Germany'):
+    query = f"""SELECT year,count FROM population_total WHERE country_name='{country}' LIMIT 100"""
+    query_result = database.execute_query(query)
+    year, count = zip(*query_result)
     fig = Figure()
     axis = fig.add_subplot(1, 1, 1)
 
-    axis.plot(year,count)
-
-    output = io.BytesIO()
-    FigureCanvasSVG(fig).print_svg(output)
-    return Response(output.getvalue(), mimetype="image/svg+xml")
-    #return '{}'.format(query_result)
-
-
-@app.route("/matplot-as-image")
-def plot(num_x_points=50):
-    fig = Figure()
-    axis = fig.add_subplot(1, 1, 1)
-    x_points = range(num_x_points)
-    axis.plot(x_points, [random.randint(1, 30) for x in x_points])
+    axis.plot(year, count)
 
     output = io.BytesIO()
     FigureCanvasSVG(fig).print_svg(output)

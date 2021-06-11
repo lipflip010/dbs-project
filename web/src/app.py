ff926cc764db321b9af8dc9aa68078868e7f5876
lipflip010 <lipflip@posteo.de>
2021-06-11 19:02:35 +0200

    Modified plot labels and added check for empty query result

--- web/src/app.py
+++ web/src/app.py
@@ -1,11 +1,11 @@
-import time
-import redis
-import database
 import io
-import random
+
+import redis
 from flask import Flask, Response, request
 from matplotlib.backends.backend_svg import FigureCanvasSVG
 from matplotlib.figure import Figure
+
+import database
 
 app = Flask(__name__)
 cache = redis.Redis(host='redis', port=6379)
@@ -16,10 +16,18 @@ def population_total():
     country = request.args.get('country')
     query = f"""SELECT year,count FROM population_total WHERE country_name='{country}' LIMIT 100"""
     query_result = database.execute_query(query)
+    print(query_result)
+    if not query_result:
+        return "Null"
+
     year, count = zip(*query_result)
     fig = Figure()
-    axis = fig.add_subplot(1, 1, 1)
 
+    axis = fig.add_subplot(1, 1, 1)
+    axis.set_title(f"Population of {country}")
+    axis.set_xlabel("Year")
+    axis.set_ylabel("Population")
+    axis.ticklabel_format(useOffset=False, style='plain')
     axis.plot(year, count)
 
     output = io.BytesIO()

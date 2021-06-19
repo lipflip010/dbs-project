cdf9a846593f1cb6474f271b3758a600c75c5fae
lipflip010 <lipflip@posteo.de>
2021-06-19 12:13:31 +0200

    Fixed syntax error

--- web/src/app.py
+++ web/src/app.py
@@ -1,5 +1,4 @@
 from flask import Flask, Response, request, render_template
-from matplotlib.figure import Figure
 
 from plots import create_svg, get_population_total_plot_for
 
@@ -16,7 +15,7 @@ def population_total():
     country = request.args.get('country')
 
     try:
-        figure: Figure = get_population_total_plot_for(country)
+        figure = get_population_total_plot_for(country)
         return Response(create_svg(figure), mimetype="image/svg+xml")
     except:
         return f"""Country '{country}' not found""", 404

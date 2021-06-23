fd9ef8c77eacf35fb100cf1252300a1dfbbe3336
lipflip010 <lipflip@posteo.de>
2021-06-23 15:46:06 +0200

    Added co2_per_captita view

--- db/scripts/1_init.sql
+++ db/scripts/1_init.sql
@@ -1,4 +1,4 @@
-CREATE TABLE population_total ( 
+CREATE TABLE population_total (
   Country_Name varchar(40),
   Year int,
   Count int,
@@ -12,3 +12,21 @@ CREATE TABLE co2_emission (
   Emission float,
    PRIMARY KEY(Country_Name, Year)
 );
+
+CREATE TABLE gdp (
+  Country_Name varchar(80),
+  Code varchar(10),
+  Year int,
+  Value float,
+   PRIMARY KEY(Country_Name, Year)
+);
+
+CREATE VIEW co2_per_capita AS
+SELECT pt.country_name  AS country_name,
+       pt.year          AS year,
+       emission / count AS emission_per_capita
+FROM co2_emission co2
+         INNER JOIN population_total pt
+                    ON co2.country_name = pt.country_name
+                        AND co2.year = pt.year
+WHERE co2.year >= 1960

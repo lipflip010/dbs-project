874ad336e9f30a2f81c72ab0397965c988f0314f
lipflip010 <lipflip@posteo.de>
2021-06-23 16:24:17 +0200

    Added ability to compare to countries

--- web/src/static/script.js
+++ web/src/static/script.js
@@ -1,12 +1,12 @@
 const images = ['population-total','co2-emission','co2-per-capita']
 
-const setCountry = () => {
-    const form = document.forms['mainForm']
+const setCountry = (column) => {
+    const form = document.forms[`columnForm-${column}`]
     const country = form.country.value
 
     images.map((name) => {
         const url = `http://localhost:5000/${name}?country=${country}`
-        document.querySelector(`#${name}-container`).src = url
+        document.querySelector(`#${name}-container-${column}`).src = url
     })
 }
 

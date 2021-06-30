507d022b0545fbe848b7ad03972c30c174e40266
lipflip010 <lipflip@posteo.de>
2021-06-30 10:45:08 +0200

    Added cleanup script to get rid of data earlier than 1990

---
+++ db/scripts/3_cleanup.sql
@@ -0,0 +1,5 @@
+DELETE FROM co2_emission WHERE year<1990;
+DELETE FROM gdp WHERE year<1990;
+DELETE FROM population_total WHERE year<1990;
+DELETE FROM population_growth WHERE year<1990;
+DELETE FROM renewable_energy_consumption WHERE year<1990;
